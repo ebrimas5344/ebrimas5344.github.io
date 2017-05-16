f49be5c167f115d74deae41a00ad294c5a0db5c8
ebrimas5344 <ebrimas5344@kippnyccp.org>
2017-05-16 11:37:11 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   $('button').click(function(){
-    $('p').css('color','purple');
+    $('img').css('width','600px');
   });
+  
+  $(document).ready(function(){
+  $('button').click(function(){
+    $('p').css('color','white');
 });
