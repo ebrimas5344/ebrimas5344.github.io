257dfb295b0f0d6be86201a477cececacd80e7da
ebrimas5344 <ebrimas5344@kippnyccp.org>
2017-05-18 11:39:48 -0400

    Update application.js

--- guess-my-number/application.js
+++ guess-my-number/application.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
 	var random_num = Math.floor((Math.random()*100) + 1);
 	//create click event for submit button
- 
-
-
+       	$("#submit").click(function(){
+         //what did the user guess?
+		var guess= $("input[name=user-guess]").val();
+		$("#response).html('You guessed: ' + guess);
+	//is that the correct guess?
+	
+	
+       });
 	//create click event for clear button
-
+        
 
 });
