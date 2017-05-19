2aba8cb613384de4413d1b09f632ae5c91c4cdd8
ebrimas5344 <ebrimas5344@kippnyccp.org>
2017-05-19 11:34:54 -0400

    Update application.js

--- guess-my-number/application.js
+++ guess-my-number/application.js
@@ -20,6 +20,18 @@ $(document).ready(function(){
 		$("input[name=user-guess]").val('');
        });
 	//create click event for clear button
-        
-
+        var random_num = Math.floor((Math.random()*100) + 1);
+       	$("#clear").click(function(){
+	if(guess==random_num) {
+			$("#response").fadeOut().html('').fadeIn();
+		}
+		
+		else if(guess<random_num) {
+			$("#response").fadeOut().html('').fadeIn();
+		}
+		
+		else{
+			$("#response").fadeOut().html('').fadeIn();
+		}
+		$("input[name=user-guess]").val('');
 });
